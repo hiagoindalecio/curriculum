6ee56e8b7b559c3ba83da1e2c60f83825cef9439
hiagoindalecio <60201082+hiagoindalecio@users.noreply.github.com>
2020-07-27 19:32:45 -0300

    Updates

---
+++ precache-manifest.e6a97169a1aaf9dea3b85c1e6e68ee69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dc1d62d3b087ba167b523ac6501f67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b725753e03b111ff4e5",
+    "url": "./static/css/main.d58f36c1.chunk.css"
+  },
+  {
+    "revision": "e07fb5ef0e1507ec22ef",
+    "url": "./static/js/2.d44309c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d44309c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b725753e03b111ff4e5",
+    "url": "./static/js/main.eceae6ba.chunk.js"
+  },
+  {
+    "revision": "acdc90765f75b1f7aeda",
+    "url": "./static/js/runtime-main.1d53a21f.js"
+  },
+  {
+    "revision": "6a1008fc30d0ac2eee94ae685c4b5c44",
+    "url": "./static/media/curriculum.6a1008fc.svg"
+  },
+  {
+    "revision": "ce30b4849401eaf366bcaef7e4fef04c",
+    "url": "./static/media/men.ce30b484.svg"
+  }
+]);
