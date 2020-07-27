f7732b87dfd318c9662831bdbdcc0d49f77cdcff
hiagoindalecio <60201082+hiagoindalecio@users.noreply.github.com>
2020-07-27 19:48:58 -0300

    Update index.tsx

--- src/components/Qualifications/index.tsx
+++ src/components/Qualifications/index.tsx
@@ -8,7 +8,7 @@ const Qualifications = () => {
             </legend>
             <div className="field">
                 <p>
-                    Focado, esforçado, proativo e autodidata. Sou receptivo, me comunico com facilidade e cumpro com minhas obrigações. Possuo conhecimentos em hardware e desenvolvimento em JavaScript/Node.js, React, ReactNative, C, C#.NET e Python 3; banco de dados MySQL e SQL Server. Inglês intermediário.
+                    Focado, esforçado, proativo e autodidata. Sou receptivo, me comunico com facilidade e cumpro com minhas obrigações. Possuo conhecimentos em hardware e desenvolvimento em JavaScript/Node.js, React, ReactNative, C, C#.NET e Python 3; banco de dados MySQL e SQL Server. Inglês avançado.
                 </p>
             </div>
         </fieldset>
